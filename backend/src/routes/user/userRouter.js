b1d3e8bd8206b5a48fe99343de38639ecf245aae
Victor Davydenko <acc.davydenko@gmail.com>
2024-03-12 20:09:39 +0200

    add validation middleware and schemas

--- backend/src/routes/user/userRouter.js
+++ backend/src/routes/user/userRouter.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import UserController from '../../controllers/UserController.js';
+import { validationMiddleware } from '../../middlewares/validationMiddleware.js';
+import { createUserValidationSchema } from '../../validationSchemas/validationSchemas.js';
 
 const userRouter = Router();
 
-userRouter.post('/signup', UserController.registration);
+userRouter.post('/signup', validationMiddleware(createUserValidationSchema), UserController.registration);
 
 export default userRouter;
