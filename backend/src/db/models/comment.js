6428ef58f3a1422d5cd177f8aeb3b52225d623da
Victor Davydenko <acc.davydenko@gmail.com>
2024-03-19 18:32:04 +0200

    add post and comment models

---
+++ backend/src/db/models/comment.js
@@ -0,0 +1,30 @@
+import mongoose, { Schema } from 'mongoose';
+
+export const CommentSchema = new Schema({
+  text: String,
+  media: [{
+    type: Schema.Types.String,
+  }],
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  likes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  views: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  comments: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Comment',
+  }],
+}, { timestamps: true });
+
+export const CommentModel = mongoose.model('Comment', CommentSchema);
---
+++ backend/src/db/models/post.js
@@ -0,0 +1,30 @@
+import mongoose, { Schema } from 'mongoose';
+
+const PostSchema = new Schema({
+  text: String,
+  media: [{
+    type: Schema.Types.String,
+  }],
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  likes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  views: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  comments: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Comment',
+  }],
+}, { timestamps: true });
+
+export const PostModel = mongoose.model('Post', PostSchema);
