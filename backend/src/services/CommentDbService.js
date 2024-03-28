9c073c6f6bb3e9a0ebed97b01579556409919c8d
Victor Davydenko <acc.davydenko@gmail.com>
2024-03-28 17:44:58 +0200

    add error handling in database services

--- backend/src/services/CommentDbService.js
+++ backend/src/services/CommentDbService.js
@@ -1,39 +1,68 @@
 import { PostModel } from '../db/models/post.js';
 import { CommentModel } from '../db/models/comment.js';
+import DbError from '../exceptions/dbError.js';
 
 class CommentDbService {
   comment = async (comment, parentPostId, userId) => {
-    const newComment = await CommentModel.create({ ...comment, author: userId });
-    if (!newComment) {
-      throw new Error('something went wrong');
+    try {
+      const newComment = await CommentModel.create({ ...comment, author: userId });
+      if (!newComment) {
+        throw DbError.FailedToFetch();
+      }
+      const updatedPost = await PostModel.findByIdAndUpdate(parentPostId, { $push: { comments: newComment.id } });
+      if (!updatedPost) {
+        await CommentModel.findByIdAndUpdate(parentPostId, { $push: { comments: newComment.id } });
+      }
+      return newComment;
+    } catch (e) {
+      throw DbError.FailedToFetch();
     }
-    const updatedPost = await PostModel.findByIdAndUpdate(parentPostId, { $push: { comments: newComment.id } });
-    if (!updatedPost) {
-      await CommentModel.findByIdAndUpdate(parentPostId, { $push: { comments: newComment.id } });
-    }
-    return newComment;
   };
 
   getComment = async (id) => {
-    const comment = CommentModel.findById(id)
-      .populate('comments');
-    return comment;
+    try {
+      const comment = await CommentModel.findById(id)
+        .populate('comments');
+      return comment;
+    } catch (e) {
+      throw DbError.FailedToFetch();
+    }
   };
 
   deleteComment = async (id) => {
-    await CommentModel.findByIdAndDelete(id);
+    try {
+      const comment = await CommentModel.findByIdAndDelete(id);
+      return comment;
+    } catch (e) {
+      throw DbError.FailedToFetch();
+    }
   };
 
   likeComment = async (commentId, userId) => {
-    await CommentModel.findByIdAndUpdate(commentId, { $push: { likes: userId } });
+    try {
+      const comment = await CommentModel.findByIdAndUpdate(commentId, { $push: { likes: userId } });
+      return comment;
+    } catch (e) {
+      throw DbError.FailedToFetch();
+    }
   };
 
   unlikeComment = async (commentId, userId) => {
-    await CommentModel.findByIdAndUpdate(commentId, { $pull: { likes: userId } });
+    try {
+      const comment = await CommentModel.findByIdAndUpdate(commentId, { $pull: { likes: userId } });
+      return comment;
+    } catch (e) {
+      throw DbError.FailedToFetch();
+    }
   };
 
   addView = async (commentId, userId) => {
-    await CommentModel.findByIdAndUpdate(commentId, { $push: { views: userId } });
+    try {
+      const comment = await CommentModel.findByIdAndUpdate(commentId, { $push: { views: userId } });
+      return comment;
+    } catch (e) {
+      throw DbError.FailedToFetch();
+    }
   };
 }
 
