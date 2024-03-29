e89e2ee4917f931142bb2a9abe3b6f75a25b1354
Victor Davydenko <acc.davydenko@gmail.com>
2024-03-29 19:20:23 +0200

    add chat controller and services

---
+++ backend/src/services/ChatDbService.js
@@ -0,0 +1,52 @@
+import { ChatModel } from '../db/models/chat.js';
+import DbError from '../exceptions/dbError.js';
+import ChatError from '../exceptions/chatError.js';
+import { MessageModel } from '../db/models/message.js';
+
+class ChatDbService {
+  newChat = async (members) => {
+    try {
+      await ChatModel.create({ members });
+    } catch (e) {
+      if (e.message === 'Chat already exists') {
+        throw ChatError.AlreadyExists(400, 'The chat with these users already exists');
+      } else {
+        throw DbError.FailedToFetch();
+      }
+    }
+  };
+
+  getChats = async (id) => {
+    try {
+      const chats = ChatModel.find({
+        members: {
+          $in: [id],
+        },
+      });
+      return chats;
+    } catch (e) {
+      throw DbError.FailedToFetch();
+    }
+  };
+
+  newMessage = async (message) => {
+    try {
+      const newMessage = await MessageModel.create({ ...message });
+      return newMessage;
+    } catch (e) {
+      throw DbError.FailedToFetch();
+    }
+  };
+
+  getMessages = async (chatId) => {
+    try {
+      console.log(chatId);
+      const messages = MessageModel.find({ chatId });
+      return messages;
+    } catch (e) {
+      throw DbError.FailedToFetch();
+    }
+  };
+}
+
+export default new ChatDbService();
