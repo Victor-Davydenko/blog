892263c7459df3e60d84923190acadacfc40396e
Victor Davydenko <acc.davydenko@gmail.com>
2024-03-30 21:15:58 +0200

    fix chat avatar

--- frontend/src/components/Chat/ChatListItem.js
+++ frontend/src/components/Chat/ChatListItem.js
@@ -2,6 +2,7 @@ import React, {useContext, useEffect} from 'react';
 import {toJS} from "mobx";
 import Context from "../../index";
 import {observer} from "mobx-react-lite";
+import userPic from '../../assets/default.jpg'
 
 const ChatListItem = ({ chat, onChatPick }) => {
   const { userStore, chatStore: {getChatCompanion, companion} } = useContext(Context);
@@ -11,10 +12,11 @@ const ChatListItem = ({ chat, onChatPick }) => {
   useEffect(() => {
     getChatCompanion(companionId)
   }, []);
+  const avatar = myCompanion.avatar ? `${process.env.REACT_APP_BASE_URI}/uploads/${myCompanion.avatar}` : userPic;
   return (
     myCompanion ?
       <li onClick={() => onChatPick(chat._id)} className='chats_list_item'>
-      <img src={`${process.env.REACT_APP_BASE_URI}/uploads/${myCompanion.avatar}`} alt="avatar" className='userpic'/>
+      <img src={avatar} alt="avatar" className='userpic'/>
       <span className='username'>{myCompanion.username}</span>
       </li>
       : null
